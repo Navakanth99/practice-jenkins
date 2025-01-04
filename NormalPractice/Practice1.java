9e9f2afb12637d5c65f02000dc44bd2285172303
Navakanth99 <gundaramnavakanth@gmail.com>
2025-01-04 17:06:04 +0530

    changed

--- NormalPractice/Practice1.java
+++ NormalPractice/Practice1.java
@@ -2,6 +2,6 @@ public Practice1{
     public static void main(String args[]){
       
       System.out.println("1st line excuted");
-      
+      System.out.println("2nd line executed") 
     }
 }
